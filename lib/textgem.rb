3bfe9d653a3daf57a5c4d5fa48ebdc6c31649ecf
Da2dalus <63458929+Da2dalus@users.noreply.github.com>
2021-04-05 19:19:26 +0200

    Update textgem.rb

--- lib/textgem.rb
+++ lib/textgem.rb
@@ -1,5 +1,4 @@
 require "textgem/version"
-require "textgem/config"
 require "json"
 
 include JSON
@@ -9,30 +8,60 @@ module Textgem
   class Error < StandardError; end
 
   def sendsms(phone, sms, apikey="textbelt", webhook="textbelt.com")
+
+    curl = "curl" # Path to curl or the command that invokes it
+    # curl = "/bin/curl" # For Unix
+
+    textbelt = "https://textbelt.com" # Textbelt server
+
     smsoutput = `#{curl} -X POST #{textbelt}/text --data-urlencode phone='#{phone}' --data-urlencode message='#{sms}' -d replyWebhookUrl='https://#{webhook}/api/handleSmsReply' -d key=#{apikey}`
     puts smsoutput
     sms = JSON.parse(smsoutput)
   end
 
   def quota(apikey="textbelt")
+
+    curl = "curl" # Path to curl or the command that invokes it
+    # curl = "/bin/curl" # For Unix
+
+    textbelt = "https://textbelt.com" # Textbelt server
+
     quotaoutput = `#{curl} #{textbelt}/quota/#{apikey}`
     puts quotaoutput
     quota = JSON.parse(quotaoutput)
   end
 
   def status(textid)
+
+    curl = "curl" # Path to curl or the command that invokes it
+    # curl = "/bin/curl" # For Unix
+
+    textbelt = "https://textbelt.com" # Textbelt server
+
     statusoutput = `#{curl} #{textbelt}/status/#{textid}`
     puts statusoutput
     status = JSON.parse(statusoutput)
   end
 
   def repliecheck(webhook)
-    replieoutput = `#{curl} https://#{webhook}/api/handleSmsReply`
+
+    curl = "curl" # Path to curl or the command that invokes it
+    # curl = "/bin/curl" # For Unix
+
+    textbelt = "https://textbelt.com" # Textbelt server
+
+    replieoutput = `#{curl} #{webhook}/api/handleSmsReply`
     puts replieoutput
     replie = JSON.parse(replieoutput)
   end
 
   def testsms(apikey="textbelt")
+
+    curl = "curl" # Path to curl or the command that invokes it
+    # curl = "/bin/curl" # For Unix
+
+    textbelt = "https://textbelt.com" # Textbelt server
+
     testsmsoutput = `#{curl} -X POST #{textbelt}/text --data-urlencode phone='304033489438938' --data-urlencode message='hi' -d key=#{apikey}_test`
     puts testsmsoutput
     testsms = JSON.parse(testsmsoutput)
