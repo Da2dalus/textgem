5e9575597a67b44fd04830e40d47acf3b1bb3b1b
Da2dalus <63458929+Da2dalus@users.noreply.github.com>
2021-04-05 19:05:20 +0200

    Update textgem.rb

--- lib/textgem.rb
+++ lib/textgem.rb
@@ -1,39 +1,40 @@
 require "textgem/version"
+require "textgem/config"
 require "json"
 
 include JSON
+include Textgem
 
 module Textgem
   class Error < StandardError; end
 
   def sendsms(phone, sms, apikey="textbelt", webhook="textbelt.com")
-    smsoutput = `curl -X POST https://textbelt.com/text --data-urlencode phone='#{phone}' --data-urlencode message='#{sms}' -d replyWebhookUrl='https://#{webhook}/api/handleSmsReply' -d key=#{apikey}`
+    smsoutput = `#{curl} -X POST #{textbelt}/text --data-urlencode phone='#{phone}' --data-urlencode message='#{sms}' -d replyWebhookUrl='https://#{webhook}/api/handleSmsReply' -d key=#{apikey}`
     puts smsoutput
     sms = JSON.parse(smsoutput)
   end
 
   def quota(apikey="textbelt")
-    quotaoutput = `curl https://textbelt.com/quota/#{apikey}`
+    quotaoutput = `#{curl} #{textbelt}/quota/#{apikey}`
     puts quotaoutput
     quota = JSON.parse(quotaoutput)
   end
 
   def status(textid)
-    statusoutput = `curl https://textbelt.com/status/#{textid}`
+    statusoutput = `#{curl} #{textbelt}/status/#{textid}`
     puts statusoutput
     status = JSON.parse(statusoutput)
   end
 
   def repliecheck(webhook)
-    replieoutput = `curl https://#{webhook}/api/handleSmsReply`
+    replieoutput = `#{curl} https://#{webhook}/api/handleSmsReply`
     puts replieoutput
     replie = JSON.parse(replieoutput)
   end
 
   def testsms(apikey="textbelt")
-    testsmsoutput = `curl -X POST https://textbelt.com/text --data-urlencode phone='304033489438938' --data-urlencode message='hi' -d key=#{apikey}_test`
+    testsmsoutput = `#{curl} -X POST #{textbelt}/text --data-urlencode phone='304033489438938' --data-urlencode message='hi' -d key=#{apikey}_test`
     puts testsmsoutput
     testsms = JSON.parse(testsmsoutput)
   end
 end
-
