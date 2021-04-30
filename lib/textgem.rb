4dfa5935900f0b2cd09deb55355dae5c23700387
Da2dalus <63458929+Da2dalus@users.noreply.github.com>
2021-04-30 14:50:29 +0200

    Update textgem.rb

--- lib/textgem.rb
+++ lib/textgem.rb
@@ -4,65 +4,46 @@ require "json"
 include JSON
 include Textgem
 
-module Textgem
+class Textgem
   class Error < StandardError; end
-
-  def sendsms(phone, sms, apikey="textbelt", webhook="textbelt.com")
-
-    curl = "curl" # Path to curl or the command that invokes it
-    # curl = "/bin/curl" # For Unix
-
-    textbelt = "https://textbelt.com" # Textbelt server
-
-    smsoutput = `#{curl} -X POST #{textbelt}/text --data-urlencode phone='#{phone}' --data-urlencode message='#{sms}' -d replyWebhookUrl='https://#{webhook}/api/handleSmsReply' -d key=#{apikey}`
+  
+  def initialize(curl="curl", textbelt="https://textbelt.com")
+    @@curl = curl
+    @@textbelt = textbelt
+    
+  end
+  
+  def self.sendsms(phone, sms, apikey="textbelt", webhook="textbelt.com")
+    
+    smsoutput = `#{@@curl} -X POST #{@@textbelt}/text --data-urlencode phone='#{phone}' --data-urlencode message='#{sms}' -d replyWebhookUrl='https://#{webhook}/api/handleSmsReply' -d key=#{apikey}`
     puts smsoutput
     sms = JSON.parse(smsoutput)
   end
 
-  def quota(apikey="textbelt")
-
-    curl = "curl" # Path to curl or the command that invokes it
-    # curl = "/bin/curl" # For Unix
-
-    textbelt = "https://textbelt.com" # Textbelt server
-
-    quotaoutput = `#{curl} #{textbelt}/quota/#{apikey}`
+  def self.quota(apikey="textbelt")
+    
+    quotaoutput = `#{@@curl} #{@@textbelt}/quota/#{apikey}`
     puts quotaoutput
     quota = JSON.parse(quotaoutput)
   end
 
-  def status(textid)
-
-    curl = "curl" # Path to curl or the command that invokes it
-    # curl = "/bin/curl" # For Unix
-
-    textbelt = "https://textbelt.com" # Textbelt server
-
-    statusoutput = `#{curl} #{textbelt}/status/#{textid}`
+  def self.status(textid)
+    
+    statusoutput = `#{@@curl} #{@@textbelt}/status/#{textid}`
     puts statusoutput
     status = JSON.parse(statusoutput)
   end
 
-  def repliecheck(webhook)
-
-    curl = "curl" # Path to curl or the command that invokes it
-    # curl = "/bin/curl" # For Unix
-
-    textbelt = "https://textbelt.com" # Textbelt server
-
-    replieoutput = `#{curl} #{webhook}/api/handleSmsReply`
+  def self.repliecheck(webhook)
+    
+    replieoutput = `#{@@curl} #{@@webhook}/api/handleSmsReply`
     puts replieoutput
     replie = JSON.parse(replieoutput)
   end
 
-  def testsms(apikey="textbelt")
-
-    curl = "curl" # Path to curl or the command that invokes it
-    # curl = "/bin/curl" # For Unix
-
-    textbelt = "https://textbelt.com" # Textbelt server
-
-    testsmsoutput = `#{curl} -X POST #{textbelt}/text --data-urlencode phone='304033489438938' --data-urlencode message='hi' -d key=#{apikey}_test`
+  def self.testsms(apikey="textbelt")
+    
+    testsmsoutput = `#{@@curl} -X POST #{@@textbelt}/text --data-urlencode phone='304033489438938' --data-urlencode message='hi' -d key=#{apikey}_test`
     puts testsmsoutput
     testsms = JSON.parse(testsmsoutput)
   end
